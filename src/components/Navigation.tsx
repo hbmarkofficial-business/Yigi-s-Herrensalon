541744bfb6403d0a7b45518cd0bc40c3d009cbb4
hbmarkofficial-business <hbmark.official@gmail.com>
2026-02-08 14:32:02 +0100

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -45,10 +45,10 @@ export const Navigation = () => {
           <Scissors className="w-8 h-8 text-primary transition-transform duration-300 group-hover:rotate-45" />
           <div className="flex flex-col">
             <span className="font-display text-xl font-bold text-foreground tracking-wide">
-            Top
+            Yigi's
             </span>
             <span className="text-primary text-sm tracking-[0.3em] uppercase -mt-1">
-              Salon
+              Herrensalon
             </span>
           </div>
         </Link>
